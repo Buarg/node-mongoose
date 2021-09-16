014d129b902b8e6358630b0efde197b3a684a9cc
buarg <hector_buarg@hotmail.com>
2021-09-16 18:19:26 +0200

    Mongoose part two

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
    console.log('Connected correctly to the server');
 
-   let newDish = Dishes({
+   Dishes.create({
        name: 'Uthappizza',
        description: 'test'
-   });
-
-   newDish.save().then((dish) => {
+   }).then((dish) => {
        console.log(dish);
 
        return Dishes.find({}).exec();
