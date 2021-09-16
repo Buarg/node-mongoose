349b5daebdf33c936ce3b389db912029d1b35123
buarg <hector_buarg@hotmail.com>
2021-09-16 18:39:28 +0200

    Mongoose part three

--- models/dishes.js
+++ models/dishes.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
-const {model} = require("mongoose");
+const {model, Schema} = require("mongoose");
 
 const schema = mongoose.Schema;
+
+const commentSchema = new schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
 const dishSchema = new schema({
     name: {
@@ -12,7 +31,8 @@ const dishSchema = new schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [ commentSchema ]
 }, {
     timestamps: true
 });
